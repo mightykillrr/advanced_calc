a3605769feadbeb53681260d2eb916fa22d4f4f0
mightykillrr <aniket1010104@gmail.com>
2021-04-23 15:22:43 +0530

    optimised code

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
 import math
-from pandas import DataFrame
+# from pandas import DataFrame
 from time import sleep
 
 round_val = 5
@@ -9,34 +9,20 @@ round_val = 5
 def options():
     print("So, What are we doing today?")
     print("Choose an option from the list below: ")
-    print("[+] Simple")
-    print("    1. Add")
-    print("    2. Subtract")
-    print("    3. Multiply")
+    print("[+] Simple                                   [+]Trigno")
+    print("    1. Add                                       9. sin(a), a in degrees")
+    print("    2. Subtract                                 10. cos(a), a in degrees")
+    print("    3. Multiply                                 11. tan(a), a in degrees")
     print("    4. Divide")
     print(" ")
-    print("[+]Percentage")
-    print("    5. Percent of(a% of b)")
-    print("    6. Percent in(% of a in b)")
-    print(" ")
+    print("[+]Percentage                                [+]Shapes: Gives Perimeter, Area and Diagonal/Diameter")
+    print("    5. Percent of(a% of b)                      12. Square")
+    print("    6. Percent in(% of a in b)                  13. Rectangle")
+    print("                                                14. Circle")
     print("[+]Advanced")
-    print("    7. Power of(a^b)")
-    print("    8. Square Root(√a)")
-    print(" ")
-    print("[+]Trigno")
-    print("    9. sin(a), a in degrees")
-    print("   10. cos(a), a in degrees")
-    print("   11. tan(a), a in degrees")
-    print(" ")
-    print("[+]Shapes: Gives Perimeter, Area and Diagonal/Diameter")
-    print("   12. Square")
-    print("   13. Rectangle")
-    print("   14. Circle")
-    print(" ")
-    print(" ")
-    print("[+]Info and More")
-    print("   15. About this Program")
-    print("   16. About Creator")
+    print("    7. Power of(a^b)                         [+]Info and More")
+    print("    8. Square Root(√a)                          15. About this Program")
+    print("                                                16. About Creator")
 
 
 # addition module
@@ -142,17 +128,17 @@ def percentof():
     print("[%] Percent of(a% of b)")
     print("Example: a% of b")
     print(" ")
-    a = 1
-    b = 1
+    perval1 = 1
+    perval2 = 1
     math_on = True
     while math_on:
         try:
-            a = float(input("[%] Enter first value(a): "))
-            b = float(input("[%] Enter second value(b): "))
+            perval1 = float(input("[%] Enter first value(a): "))
+            perval2 = float(input("[%] Enter second value(b): "))
             math_on = False
         except ValueError:
             print("Please enter a number!")
-    return [str(a), str(b)], round(((a / 100) * b), round_val)
+    return [str(perval1), str(perval2)], round(((perval2 / 100) * perval2), round_val)
 
 
 # percent in module - % of a in b
@@ -160,17 +146,17 @@ def percentin():
     print("[%] Percent in (% of a in b)")
     print("Example: % of a in b")
     print(" ")
-    a = 1
-    b = 1
+    perinval1 = 1
+    perinval2 = 1
     math_on = True
     while math_on:
         try:
-            a = float(input("[%] Enter the first value(a): "))
-            b = float(input("[%] Enter the second value(b): "))
+            perinval1 = float(input("[%] Enter the first value(a): "))
+            perinval2 = float(input("[%] Enter the second value(b): "))
             math_on = False
         except ValueError:
             print("Please enter a number!")
-    return [a, b], round(((a / b) * 100), round_val)
+    return [perinval1, perinval2], round(((perinval1 / perinval2) * 100), round_val)
 
 
 # power of where a^b
@@ -178,17 +164,17 @@ def power():
     print("[^] Power of(a^b)")
     print("Example: a^b or a to the power of b")
     print(" ")
-    a = 1
-    b = 1
+    pwr1 = 1
+    pwr2 = 1
     math_on = True
     while math_on:
         try:
-            a = float(input("[^] Enter the Base(a): "))
-            b = float(input("[^] Enter the Power(b): "))
+            pwr1 = float(input("[^] Enter the Base(a): "))
+            pwr2 = float(input("[^] Enter the Power(b): "))
             math_on = False
         except ValueError:
             print("Please enter a number!")
-    return [a, b], a ** b
+    return [pwr1, pwr2], pwr1 ** pwr2
 
 
 # square root
@@ -196,15 +182,15 @@ def sqrt():
     print("[√] Square Root(√a)")
     print("Example: √a")
     print(" ")
-    a = 1
+    sqrtval = 1
     math_on = True
     while math_on:
         try:
-            a = float(input("[√] Enter the value(a): "))
+            sqrtval = float(input("[√] Enter the value(a): "))
             math_on = False
         except ValueError:
             print("Please enter a number!")
-    return [a], round(a ** 0.5, round_val)
+    return [sqrtval], round(sqrtval ** 0.5, round_val)
 
 
 # sin
@@ -212,15 +198,15 @@ def sin():
     print("[O] sin(a), a in degrees")
     print("Example: sin(a) where a is in degrees")
     print(" ")
-    a = 1
+    sinval = 1
     math_on = True
     while math_on:
         try:
-            a = float(input("[O] Enter the value(a): "))
+            sinval = float(input("[O] Enter the value(a): "))
             math_on = False
         except ValueError:
             print("Please enter a number!")
-    return [a], round(math.sin(math.radians(a)), round_val)
+    return [sinval], round(math.sin(math.radians(sinval)), round_val)
 
 
 # cos
@@ -228,15 +214,15 @@ def cos():
     print("[O] cos(a), a in degrees")
     print("Example: cos(a) where a is in degrees")
     print(" ")
-    a = 1
+    cosval = 1
     math_on = True
     while math_on:
         try:
-            a = float(input("[O] Enter the value(a): "))
+            cosval = float(input("[O] Enter the value(a): "))
             math_on = False
         except ValueError:
             print("Please enter a number!")
-    return [a], round(math.cos(math.radians(a)), round_val)
+    return [cosval], round(math.cos(math.radians(cosval)), round_val)
 
 
 # tan
@@ -244,45 +230,45 @@ def tan():
     print("[O] tan(a), a in degrees")
     print("Example: tan(a) where a is in degrees")
     print(" ")
-    a = 1
+    tanval = 1
     math_on = True
     while math_on:
         try:
-            a = float(input("[O] Enter the value(a): "))
+            tanval = float(input("[O] Enter the value(a): "))
             math_on = False
         except ValueError:
             print("Please enter a number!")
-    return [a], round(math.tan(math.radians(a)), round_val)
+    return [tanval], round(math.tan(math.radians(tanval)), round_val)
 
 
 # square- perimeter, area, diagonal
 def square():
     print("[-] Square")
-    a = 1
+    squareval = 1
     math_on = True
     while math_on:
         try:
-            a = float(input("[=] Enter the length of side of the square(a): "))
+            squareval = float(input("[=] Enter the length of side of the square(a): "))
             math_on = False
         except ValueError:
             print("Please enter a number!")
-    return [a], 4 * a, a ** 2, round((2 ** 0.5) * a, round_val)
+    return [squareval], 4 * squareval, squareval ** 2, round((2 ** 0.5) * squareval, round_val)
 
 
 # rectangle- perimeter, area, diagonal
 def rectangle():
     print("[-] Rectangle")
-    a = 1
-    b = 1
+    ln = 1
+    wd = 1
     math_on = True
     while math_on:
         try:
-            a = float(input("[=] Enter the length of rectangle(l): "))
-            b = float(input("[=] Enter the width of rectangle(w): "))
+            ln = float(input("[=] Enter the length of rectangle(l): "))
+            wd = float(input("[=] Enter the width of rectangle(w): "))
             math_on = False
         except ValueError:
             print("Please enter a number!")
-    return [a, b], 2 * (a + b), a * b, round((((a ** 2) + (b ** 2)) ** 0.5), round_val)
+    return [ln, wd], 2 * (ln + wd), ln * wd, round((((ln ** 2) + (wd ** 2)) ** 0.5), round_val)
 
 
 # circle- circumference, area, diameter
@@ -318,8 +304,9 @@ def restart_app():
 
 # main code
 print("Welcome to my AdvancedCalculator!")
-print(
-    "This project is entirely made in Python as a learning milestone. If you have any suggestion, make sure to comment on this repo on GitHub!")
+print(" ")
+print("This project is entirely made in Python as a learning milestone. If you have any suggestion, make sure to "
+      "comment on this repo on GitHub!")
 print("Anyways, lets get started!")
 print(" ")
 
@@ -327,15 +314,15 @@ app_on = True
 while True:
     while app_on:
         print("All solutions given by this program are rounded off to 5 decimals as default.")
-        round_new = input(
-            "If you wish to change to your choice, enter the number, or leave it blank to use the default: ")
+        round_new = input("If you wish to change to your choice, enter the number, or leave it blank to use the "
+                          "default: ")
         if round_new == '':
-            print("The default of 5 decimal places is being used!")
+            print("The default of [5] decimal places is being used!")
             print(" ")
         else:
             try:
                 round_val = int(round_new)
-                print(f"Results are now rounded off at {round_val} decimal places.")
+                print(f"Results are now rounded off at [{round_val}] decimal places.")
                 print(" ")
             except ValueError:
                 print("Please enter an Integer value!")
@@ -431,7 +418,8 @@ while True:
                 print("\n"*100)
                 print("[+] About this program:")
                 print(" ")
-                print("This is a small, but feature filled calculator, made entirely in Python. More features to be added very soon!")
+                print("This is a small, but feature filled calculator, made entirely in Python. More features to be "
+                      "added very soon!")
                 print("I thank you from my heart for giving this program a try. I hope you enjoy it.")
                 print(" ")
                 print("[-] About the functions in the menu: ")
@@ -444,7 +432,8 @@ while True:
                 print("  [>] Percent in: Returns % of a in b. User gives the values of a and b.")
                 print("  [>] Power of: Takes  in two values, a and b, and puts in the equation -> a^b.")
                 print("  [>] Square Root: Gives the square root of the given number.")
-                print("  [>] Trignometric Functions(sin, cos, tan): Takes in a in degrees and returns their trignometric values.")
+                print("[>] Trignometric Functions(sin, cos, tan): Takes in a in degrees and returns their "
+                      "trignometric values.")
                 print("  [>] Shapes(Square, Rectangle, Circle): Takes side, length and width, or radius and returns "
                       "respective values(perimeter/ circumference, area, diagonal/diameter).")
                 print(" ")
